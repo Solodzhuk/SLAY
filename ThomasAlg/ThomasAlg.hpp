30e20aa7da1ee0cb58cfc01e55c7c2ab8213f354
Солоджук Никита <145542314+Solodzhuk@users.noreply.github.com>
2025-02-07 00:55:12 +0300

    Add files via upload

---
+++ ThomasAlg/ThomasAlg.hpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+
+std::vector<float>* ThomasAlg(const std::vector<float>* a, const std::vector<float>* b, const std::vector<float>* c, const std::vector<float>* d){
+    std::vector<float> p, q;
+    // проверка на размер матрицы иначе 不好
+    int n = b->size();
+    p.push_back(-(*c)[0]/(*b)[0]);
+    q.push_back((*d)[0]/(*b)[0]);
+    for (int i = 1; i < n-1; i++){
+        p.push_back(-(*c)[i]/((*a)[i-1]*p[i-1] + (*b)[i]));
+        q.push_back(((*d)[i] - (*a)[i-1]*q[i-1])/((*a)[i-1]*p[i-1] + (*b)[i]));
+    }
+    std::vector<float>* x;
+    std::vector<float>* xf = new std::vector<float>;
+    x->push_back(((*d)[n-1] - (*a)[n-2]*q[n-2])/((*a)[n-2]*p[n-2] + (*b)[n-1]));
+    for (int i = 1; i < n; i++){
+        x->push_back(p[n-i-1]*(*x)[i-1] + q[n-i-1]);
+    }
+
+    for (int i = 0; i < x->size(); i++){
+        xf->push_back((*x)[x->size() - i - 1]);
+    }
+    return xf;
+}   
+
+// std::vector<float>* ReadVec (int size){
+//     std::vector<float>* x = new std::vector<float>;
+//     for (int i = 0; i < size; i++){
+//         std::cin >> (*x)[i];
+//     }
+//     return x;
+// }
