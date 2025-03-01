721fb6c5da2d699531f050a6873a5b9b8f77585d
Солоджук Никита <145542314+Solodzhuk@users.noreply.github.com>
2025-03-01 22:19:29 +0300

    произошёл коммит не туда

--- Iter/Iter.hpp
+++
@@ -1,7 +0,0 @@
-#include <CSR/CSR.hpp>
-
-std::vector<double> simple_iter(CSR& A, std::vector<double>& b, const double& accuracy, const double& T, unsigned int iter_max);
-
-std::vector<double> Yakoby_iter(CSR& A, std::vector<double>& b, const double& accuracy, const double& T, unsigned int iter_max);
-
-std::vector<double> GaussSeidel_iter(CSR& A, std::vector<double>& b, const double& accuracy, const double& T, unsigned int iter_max);
